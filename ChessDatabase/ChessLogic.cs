9ce27f0e8765ca8a3e1cf1d35a27b6cc0fd93e76
Yullix <tedted93@hotmail.com>
2016-07-30 09:30:13 +0200

    Added static class ChessLogic to hold all actions and rules regarding matches and openings.

---
+++ ChessDatabase/ChessLogic.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDatabase
+{
+    public static class ChessLogic
+    {
+        /// <summary>
+        /// Returns 2 int values that represents the position of the square in a 8x8 jagged array
+        /// </summary>
+        /// <param name="pBoxName"></param>
+        /// <returns>int[2]</returns>
+        public static int[] GetSquarePos(string pBoxName)
+        {
+            string[] squareString = pBoxName.Split('x');
+
+            int[] squarePos = new int[2];
+            squarePos[0] = Int32.Parse(squareString[1][1].ToString()) - 1;
+
+            switch (squareString[1][0])
+            {
+                case 'a':
+                    squarePos[1] = 0;
+                    break;
+                case 'b':
+                    squarePos[1] = 1;
+                    break;
+                case 'c':
+                    squarePos[1] = 2;
+                    break;
+                case 'd':
+                    squarePos[1] = 3;
+                    break;
+                case 'e':
+                    squarePos[1] = 4;
+                    break;
+                case 'f':
+                    squarePos[1] = 5;
+                    break;
+                case 'g':
+                    squarePos[1] = 6;
+                    break;
+                case 'h':
+                    squarePos[1] = 7;
+                    break;
+            }
+
+            return squarePos;
+        }
+
+        /// <summary>
+        /// returns a 8x8 jagged array of strings that represents the board in the starting position
+        /// </summary>
+        /// <returns>string[8,8]</returns>
+        public static string[,] GetStartPosition()
+        {
+            string[,] startPosition = new string[8, 8];
+
+            // White starting position
+            startPosition[0, 0] = "wR";
+            startPosition[0, 1] = "wN";
+            startPosition[0, 2] = "wB";
+            startPosition[0, 3] = "wQ";
+            startPosition[0, 4] = "wK";
+            startPosition[0, 5] = "wB";
+            startPosition[0, 6] = "wN";
+            startPosition[0, 7] = "wR";
+            startPosition[1, 0] = "wP";
+            startPosition[1, 1] = "wP";
+            startPosition[1, 2] = "wP";
+            startPosition[1, 3] = "wP";
+            startPosition[1, 4] = "wP";
+            startPosition[1, 5] = "wP";
+            startPosition[1, 6] = "wP";
+            startPosition[1, 7] = "wP";
+
+            //Black starting position
+            startPosition[7, 0] = "bR";
+            startPosition[7, 1] = "bN";
+            startPosition[7, 2] = "bB";
+            startPosition[7, 3] = "bQ";
+            startPosition[7, 4] = "bK";
+            startPosition[7, 5] = "bB";
+            startPosition[7, 6] = "bN";
+            startPosition[7, 7] = "bR";
+            startPosition[6, 0] = "bP";
+            startPosition[6, 1] = "bP";
+            startPosition[6, 2] = "bP";
+            startPosition[6, 3] = "bP";
+            startPosition[6, 4] = "bP";
+            startPosition[6, 5] = "bP";
+            startPosition[6, 6] = "bP";
+            startPosition[6, 7] = "bP";
+
+            return startPosition;
+        }
+    }
+}
--- ChessDatabase/Home.cs
+++ ChessDatabase/Home.cs
@@ -43,7 +43,6 @@ namespace ChessDatabase
         private Stream blackQueenStream;
         private Stream blackKingStream;
         private MoveService _moveService;
-        private GameService _gameService;
         private RepositoryFactory _repoFactory;
         private List<Move> gameMoves;
         private Move currentMove;
@@ -57,18 +56,6 @@ namespace ChessDatabase
             private set
             {
                 _moveService = value;
-            }
-        }
-
-        public GameService gameService
-        {
-            get
-            {
-                return _gameService ?? new GameService(repoFactory);
-            }
-            private set
-            {
-                _gameService = value;
             }
         }
 
@@ -90,44 +77,8 @@ namespace ChessDatabase
 
             gameMoves = new List<Move>();
             color = "white";
-            position = new string[8, 8];
+            position = ChessLogic.GetStartPosition();
             moveNmbr = 1;
-
-            // White starting position
-            position[0, 0] = "wR";
-            position[0, 1] = "wN";
-            position[0, 2] = "wB";
-            position[0, 3] = "wQ";
-            position[0, 4] = "wK";
-            position[0, 5] = "wB";
-            position[0, 6] = "wN";
-            position[0, 7] = "wR";
-            position[1, 0] = "wP";
-            position[1, 1] = "wP";
-            position[1, 2] = "wP";
-            position[1, 3] = "wP";
-            position[1, 4] = "wP";
-            position[1, 5] = "wP";
-            position[1, 6] = "wP";
-            position[1, 7] = "wP";
-
-            //Black starting position
-            position[7, 0] = "bR";
-            position[7, 1] = "bN";
-            position[7, 2] = "bB";
-            position[7, 3] = "bQ";
-            position[7, 4] = "bK";
-            position[7, 5] = "bB";
-            position[7, 6] = "bN";
-            position[7, 7] = "bR";
-            position[6, 0] = "bP";
-            position[6, 1] = "bP";
-            position[6, 2] = "bP";
-            position[6, 3] = "bP";
-            position[6, 4] = "bP";
-            position[6, 5] = "bP";
-            position[6, 6] = "bP";
-            position[6, 7] = "bP";
 
             startSq = null;
             endSq = null;
@@ -402,7 +353,7 @@ namespace ChessDatabase
 
         private void btnSaveGame_Click(object sender, EventArgs e)
         {
-            gameService.Add(gameMoves, txtBlackPlayer.Text, txtWhitePlayer.Text, dateGameDate.Value);
+            //gameService.Add(gameMoves, txtBlackPlayer.Text, txtWhitePlayer.Text, dateGameDate.Value);
         }
 
         private void pBoxa1_Click(object sender, EventArgs e)
